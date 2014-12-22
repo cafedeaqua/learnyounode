30fe329ac61c541378511a5c9bc52729b008591a
cafedeaqua <cafedeaqua@users.noreply.github.com>
2014-12-22 21:14:37 +0900

    Complete lesson4 from master code

--- program.js
+++ program.js
@@ -1,10 +1,10 @@
 var fs=require('fs');
 
-var contents = fs.readFile(process.argv[2], 'utf8', function(err, data) {
+var contents = fs.readFile(process.argv[2], 'utf8', function(err, contents) {
     if(err){
 	throw err;
     }
-    var lines = data.toString().split('\n').length - 1;
+    var lines = contents.toString().split('\n').length - 1;
     console.log(lines);
 });
 
